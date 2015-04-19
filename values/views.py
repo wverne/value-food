2a1a9570aac9617d8dae6f81a73b25e17fe9a495
Wesley Verne <wesley@zerofox.com>
2015-04-18 22:24:18 -0400

    cleaned up sidebar

--- values/views.py
+++ values/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from sites.models import Food
 from values.models import Value
 
 
 def index(request):
     return render(request, 'values/index.html', {
-        'values': Value.objects.all()
+        'values': Value.objects.all(),
+        'foods': Food.objects.all()
     })
